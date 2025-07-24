10ab1382e8c9d7eac228d59103fffa284bc81644
caaalmvf <caaalmvf@gmail.com>
2025-07-24 03:34:21 +0000

    v.1.1.1.1

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -10,6 +10,8 @@
     # pkgs.python311Packages.pip
     pkgs.nodejs_20
     # pkgs.nodePackages.nodemon
+    pkgs.nix-search-cli
+    pkgs.envfs
   ];
   # Sets environment variables in the workspace
   env = {};
